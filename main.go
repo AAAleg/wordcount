213bd757b0b288da8c42edfc22df2800444dda55
Oleg Marintsov <awwert@yandex.ru>
2021-06-15 16:23:05 +0700

    Change implementation

--- main.go
+++ main.go
@@ -1,25 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"github.com/AAAleg/wordcount/wordCount"
-	"io"
-	"log"
 	"os"
+	"strings"
 )
 
-func consoleReader() string {
-	var reader *bufio.Reader
-
-	reader = bufio.NewReader(os.Stdin)
-	str, err := reader.ReadString('\n')
-	if err != nil && err != io.EOF {
-		log.Fatal(err)
-	}
-	return str
-}
-
 func main() {
-	fmt.Println(wordCount.WordCount(consoleReader()))
+	if len(os.Args) != 2 {
+		fmt.Println("usage: ./wordcount TEXT")
+		return
+	}
+	fmt.Println(len(strings.Fields(os.Args[1])))
+	// commit for actions
 }
